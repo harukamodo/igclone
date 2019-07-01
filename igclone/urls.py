056b36e60a7282faabd7a7f91715a84ca718b8c6
harukamodo <tri.in.the.sky@gmail.com>
2019-07-01 00:06:13 -0700

    added users app

--- igclone/urls.py
+++ igclone/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from igclone.models import *
+from django.urls import include, path
 
 urlpatterns = [
+    path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
