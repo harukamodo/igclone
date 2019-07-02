a430b11461fa1ec63c6efcc6b2b112a037b15136
harukamodo <tri.in.the.sky@gmail.com>
2019-07-02 13:30:51 -0700

    beginning working on follow views

--- users/views.py
+++ users/views.py
@@ -1,14 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from django.contrib.auth.decorators import login_required
 
+from users.models import Profile, Follower
 # Create your views here.
 
 @login_required
 def index(request): 
     return HttpResponse("Test. Here we go.")
 
-# view for following and unfollowing a user
-def Follow(request):
-    return HttpResponse("Follow me!!")
+# APIview for following and unfollowing a user
+@login_required
+class FollowView(APIView):
+    """
+    View function for following another user.
+
+    Returns the follower object upon success
+    
+    Arguments:
+    request - A WSGI Request object
+    pk - A number representing the primary key of the user being followed
+    """
+    def post(request, pk):
+        if not request.user:
+            raise ValueError('Not logged in')
+
+        if request.user.is_following_user(pk):
+            raise ValueError('You are already following this user')
+        else:
+            followed = Follower.objects.create(
+                    user_followed_id=pk,
+                    user_following=request.user.pk
+            )
+            return Response(followed.user_followed)
+
+    def delete(request, pk):
+        if not request.user:
+            raise ValueError('Not logged in')
+
+        if not request.user.is_following_user(pk):
+            raise ValueError('You must be following to unfollow.")
+        else:
+            follower_obj = request.user.get_follower_object(pk)
+            unfollowed = follower_obj.user_followed
+            follower_obj.delete()
+            return Response(unfollowed)
 
