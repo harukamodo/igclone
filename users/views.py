a9772d974665cd5d53239e392ea3e15fea3b129e
Jason Nguyen <tri.in.the.sky@gmail.com>
2019-07-02 15:54:43 -0700

    working follow and unfollow views

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.decorators import login_required
 
 from users.models import Profile, Follower
@@ -21,31 +22,49 @@ class FollowView(APIView):
     request - A WSGI Request object
     pk - A number representing the primary key of the user being followed
     """
-    @login_required
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request):
-        return HttpResponse("Test. Holy Shit. Please change.")
-    @login_required
+        if not request.user:
+            return HttpResponse('u suck')
+
+        return HttpResponse(request.user)
+
     def post(self,request):
-        if not request.user:
-            raise ValueError('Not logged in')
+        # note that this is sometimes interpretted as a __str__
+        # this is not good and if I have time I would like to
+        # write some request validator
+        # same applies for any API Call from here on with comment
+        # [Needs Validator]
+        pk = request.data.get('pk')
+        if request.data.get('pk'):
+            if request.user.is_following_user(pk):
+                raise ValueError('You are already following this user')
+            elif not Profile.objects.filter(pk=pk).exists():
+                raise ValueError('User does not exist')
+            else:
 
-        if request.user.is_following_user(pk):
-            raise ValueError('You are already following this user')
-        else:
-            followed = Follower.objects.create(
+                print('hello')
+                followed = Follower.objects.create(
                     user_followed_id=pk,
-                    user_following=request.user.pk
-            )
-            return Response({'followed' : True})
-    @login_required
-    def delete(self, request):
-        if not request.user:
-            raise ValueError('Not logged in')
-
-        if not request.user.is_following_user(pk):
-            raise ValueError('You must be following to unfollow.')
+                    user_follower_id=request.user.pk
+                )
+                return Response({'followed' : True})
         else:
-            follower_obj = request.user.get_follower_object(pk)
-            unfollowed = follower_obj.user_followed
-            follower_obj.delete()
-            return Response({'unfollowed': True})
+            raise ValueError('Pk required')
+
+    def delete(self, request):
+        # [Needs Validator]
+        pk = request.data.get('pk')
+        if request.data.get('pk'):
+            if not request.user.is_following_user(pk):
+                raise ValueError('You must be following to unfollow.')
+            elif not Profile.objects.filter(pk=pk).exists():
+                raise ValueError('User does not exist')
+            else:
+                follower_obj = request.user.get_follower_object(pk)
+                unfollowed = follower_obj.user_followed
+                follower_obj.delete()
+                return Response({'unfollowed': True})
+        else:
+            raise ValueError('Pk required')
