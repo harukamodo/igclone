4511057e08821e98bd5ee4349f51f6198742f526
Jason Nguyen <tri.in.the.sky@gmail.com>
2019-07-02 14:08:06 -0700

    small changes

--- users/views.py
+++ users/views.py
@@ -5,10 +5,9 @@ from rest_framework.views import APIView
 from django.contrib.auth.decorators import login_required
 
 from users.models import Profile, Follower
-# Create your views here.
 
 @login_required
-def index(request): 
+def index(request):
     return HttpResponse("Test. Here we go.")
 
 # APIview for following and unfollowing a user
@@ -18,12 +17,14 @@ class FollowView(APIView):
     View function for following another user.
 
     Returns the follower object upon success
-    
+
     Arguments:
     request - A WSGI Request object
     pk - A number representing the primary key of the user being followed
     """
-    def post(request, pk):
+    def get(self, request):
+        return HttpResponse("Test. Here we go.")
+    def post(self,request):
         if not request.user:
             raise ValueError('Not logged in')
 
@@ -34,17 +35,16 @@ class FollowView(APIView):
                     user_followed_id=pk,
                     user_following=request.user.pk
             )
-            return Response(followed.user_followed)
+            return Response({'followed' : True})
 
-    def delete(request, pk):
+    def delete(self, request):
         if not request.user:
             raise ValueError('Not logged in')
 
         if not request.user.is_following_user(pk):
-            raise ValueError('You must be following to unfollow.")
+            raise ValueError('You must be following to unfollow.')
         else:
             follower_obj = request.user.get_follower_object(pk)
             unfollowed = follower_obj.user_followed
             follower_obj.delete()
-            return Response(unfollowed)
-
+            return Response({'unfollowed': True})
