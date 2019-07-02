1c1d0b9d591cc73c91b9001cbf335ede55729b64
Jason Nguyen <tri.in.the.sky@gmail.com>
2019-07-02 15:54:43 -0700

    working follow and unfollow views

--- users/models.py
+++ users/models.py
@@ -44,12 +44,12 @@ class Profile(AbstractBaseUser):
     bio = models.TextField(max_length=500, null=True, blank=True)
     add_date = models.DateTimeField(auto_now_add=True, db_index=True)
     is_admin = models.BooleanField(default=False)
-    
+
     objects = ProfileManager()
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
-    
+
     def __str__(self):
         return self.email
     def has_perm(self, perm, obj=None):
@@ -62,49 +62,52 @@ class Profile(AbstractBaseUser):
 
     # returns followers in a list
     def get_followers(self):
-        followers = [ f.user_following for f in self.followers.all() ]
+        followers = [ f.user_follower for f in self.followers.all() ]
         return followers
-    
+
     # returns users profile is following in a list
     def get_following(self):
         following = [ f.user_followed for f in self.following.all() ]
         return following
-    
+
     # returns a bool depending on if another user is following this user
     def is_followed_by_user(self, pk):
-        followed_by = pk in [ f.pk for f in self.get_followers()]
+        followed_by = int(pk) in [ f.pk for f in self.get_followers()]
+        return followed_by
 
     # returns a bool depending on if this user is following another user
     def is_following_user(self, pk):
-        following =  pk in [ f.pk for f in  self.get_following()]
+        following =  int(pk) in [ f.pk for f in  self.get_following()]
         return following
 
-    # returns the following object where this user is user_followed_by
+    # returns the following object where this user is user_followed
     def get_follower_object(self, pk):
         following = None
         if self.is_following_user(pk):
             following = Follower.objects.get(
-                    user_following_pk=pk,
-                    user_followed_by=self)
+                    user_follower=self,
+                    user_followed_id=pk)
         return following
 
     def get_followed_by_object(self, pk):
         followed_by = None
         if self.is_followed_by_user(pk):
             followed_by = Follower.objects.get(
-                    user_following=self,
-                    user_followed_by_pk=pk)
+                    user_follower_id=pk,
+                    user_followed=self)
         return followed_by
 
 
 
 class Follower(models.Model):
+    #user being followed
     user_followed = models.ForeignKey(
         Profile,
         related_name="followers",
         on_delete=models.CASCADE
     )
-    user_following = models.ForeignKey(
+    #this user is the follower
+    user_follower = models.ForeignKey(
         Profile,
         related_name="following",
         on_delete=models.CASCADE
--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^', views.index, name="index"),
-    url(r'^follow/', views.FollowView.as_view(), name="follow"),
+    url(r'^$', views.index, name="index"),
+    url(r'^follow/$', views.FollowView.as_view(), name="follow"),
 ]
