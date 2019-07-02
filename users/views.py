10214af0e10d26198ff81bcd22be745ebcbe1db2
harukamodo <tri.in.the.sky@gmail.com>
2019-07-02 10:28:31 -0700

    fixed missing comment field on Comments object, beginning some testing with login required

--- users/views.py
+++ users/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def index(request): 
     return HttpResponse("Test. Here we go.")
+
+# view for following and unfollowing a user
+def Follow(request):
+    return HttpResponse("Follow me!!")
 
